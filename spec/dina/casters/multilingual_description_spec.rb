908b1788c7ef4508043e8bcd8e0fe1d0ca906c92
David Shorthouse <davidpshorthouse@gmail.com>
2022-12-22 19:30:30 -0500

    Enhance spec tests for titles and descriptions

--- spec/dina/casters/multilingual_description_spec.rb
+++ spec/dina/casters/multilingual_description_spec.rb
@@ -2,21 +2,28 @@
 module Dina
   describe 'MultilingualDescription' do
 
+    before(:each) do
+      @md = Dina::MultilingualDescription.new
+    end
+
     it "should create an object of type MultilingualDescription" do
-      md = Dina::MultilingualDescription.new
-      expect(md).to be_a(Dina::MultilingualDescription)
+      expect(@md).to be_a(Dina::MultilingualDescription)
     end
 
     it "should create an English description" do
-      md = Dina::MultilingualDescription.new
-      md.english_description = "My description"
-      expect(md.to_hash).to eq({ "descriptions" => [{ lang: "en", desc: "My description" }] })
+      @md.english_description = "My description"
+      expect(@md.to_hash).to eq({ "descriptions" => [{ lang: "en", desc: "My description" }] })
     end
 
     it "should create a French description" do
-      md = Dina::MultilingualDescription.new
-      md.french_description = "Ma description"
-      expect(md.to_hash).to eq({ "descriptions" => [{ lang: "fr", desc: "Ma description" }] })
+      @md.french_description = "Ma description"
+      expect(@md.to_hash).to eq({ "descriptions" => [{ lang: "fr", desc: "Ma description" }] })
+    end
+
+    it "should create an English & French description" do
+      @md.english_description = "My description"
+      @md.french_description = "Ma description"
+      expect(@md.to_hash).to eq({ "descriptions" => [{ lang: "en", desc: "My description" }, { lang: "fr", desc: "Ma description" }] })
     end
 
   end
--- spec/dina/casters/multilingual_title_spec.rb
+++ spec/dina/casters/multilingual_title_spec.rb
@@ -2,21 +2,28 @@
 module Dina
   describe 'MultilingualTitle' do
 
+    before(:each) do
+      @mt = Dina::MultilingualTitle.new
+    end
+
     it "should create an object of type MultilingualTitle" do
-      mt = Dina::MultilingualTitle.new
-      expect(mt).to be_a(Dina::MultilingualTitle)
+      expect(@mt).to be_a(Dina::MultilingualTitle)
     end
 
     it "should create an English title" do
-      mt = Dina::MultilingualTitle.new
-      mt.english_title = "My title"
-      expect(mt.to_hash).to eq({ "titles" => [{ lang: "en", title: "My title" }] })
+      @mt.english_title = "My title"
+      expect(@mt.to_hash).to eq({ "titles" => [{ lang: "en", title: "My title" }] })
     end
 
     it "should create a French title" do
-      mt = Dina::MultilingualTitle.new
-      mt.french_title = "Ma titre"
-      expect(mt.to_hash).to eq({ "titles" => [{ lang: "fr", title: "Ma titre" }] })
+      @mt.french_title = "Ma titre"
+      expect(@mt.to_hash).to eq({ "titles" => [{ lang: "fr", title: "Ma titre" }] })
+    end
+
+    it "should create an English & French title" do
+      @mt.english_title = "My title"
+      @mt.french_title = "Ma titre"
+      expect(@mt.to_hash).to eq({ "titles" => [{ lang: "en", title: "My title" }, { lang: "fr", title: "Ma titre" }] })
     end
 
   end
