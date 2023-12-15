bb19335799d4db22830e3afd958a25ee4383d51e
David Shorthouse <davidpshorthouse@gmail.com>
2023-12-15 13:55:03 -0500

    Crappy solution to deal with self-signed certs. Keycloak still uses RestClient

--- lib/dina/rest_client_request_patch.rb
+++ lib/dina/rest_client_request_patch.rb
@@ -1,13 +1,12 @@
-# Keycloak Client depends on RestClient & so verifying SSL must be accommodate here
+# Keycloak Client depends on RestClient & so verifying SSL must be accommodated here
+# Not a great solution, ideally should be initialized prior to calling Keycloak::Client.get_token
 
 module RestClient
   class Request
     orig_initialize = instance_method(:initialize)
   
     define_method(:initialize) do |args|
-      if Dina.config.verify_ssl == false
-        args[:verify_ssl] = false
-      end
+      args[:verify_ssl] = false
       orig_initialize.bind(self).(args)
     end
   end
